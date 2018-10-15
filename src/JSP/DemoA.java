99bda9337787c57d7a9182e361522917ace6d54a
Admin <Admin@192.168.0.3>
2018-10-15 22:56:12 +0530

    Updated

---
+++ src/JSP/DemoA.java
@@ -0,0 +1,14 @@
+package JSP;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA {
+
+	@Test
+		public void eat(){
+		Reporter.log("Hello hi boliye,jai mata di ",true);
+	}
+	
+	
+}
